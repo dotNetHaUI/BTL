87ffff2a5d597d9f8bb40f4f2f4c6a471538fe40
bezitok <nguyenhaikiller@gmail.com>
2019-11-15 13:42:17 +0700

    something

---
+++ Register.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Modul5A
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txb_FirstName = new System.Windows.Forms.TextBox();
+            this.txb_LastName = new System.Windows.Forms.TextBox();
+            this.txb_Email = new System.Windows.Forms.TextBox();
+            this.cbb_Roles = new System.Windows.Forms.ComboBox();
+            this.cbb_Office = new System.Windows.Forms.ComboBox();
+            this.txb_ComfirmPassword = new System.Windows.Forms.TextBox();
+            this.txb_Password = new System.Windows.Forms.TextBox();
+            this.dtp_DOB = new System.Windows.Forms.DateTimePicker();
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Register Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Roles";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date of birth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(105, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(105, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(107, 322);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Comfirm Password";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(105, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Office";
+            // 
+            // txb_FirstName
+            // 
+            this.txb_FirstName.Location = new System.Drawing.Point(206, 108);
+            this.txb_FirstName.Name = "txb_FirstName";
+            this.txb_FirstName.Size = new System.Drawing.Size(204, 20);
+            this.txb_FirstName.TabIndex = 9;
+            // 
+            // txb_LastName
+            // 
+            this.txb_LastName.Location = new System.Drawing.Point(206, 143);
+            this.txb_LastName.Name = "txb_LastName";
+            this.txb_LastName.Size = new System.Drawing.Size(204, 20);
+            this.txb_LastName.TabIndex = 10;
+            // 
+            // txb_Email
+            // 
+            this.txb_Email.Location = new System.Drawing.Point(206, 249);
+            this.txb_Email.Name = "txb_Email";
+            this.txb_Email.Size = new System.Drawing.Size(204, 20);
+            this.txb_Email.TabIndex = 11;
+            // 
+            // cbb_Roles
+            // 
+            this.cbb_Roles.FormattingEnabled = true;
+            this.cbb_Roles.Location = new System.Drawing.Point(206, 72);
+            this.cbb_Roles.Name = "cbb_Roles";
+            this.cbb_Roles.Size = new System.Drawing.Size(204, 21);
+            this.cbb_Roles.TabIndex = 12;
+            // 
+            // cbb_Office
+            // 
+            this.cbb_Office.FormattingEnabled = true;
+            this.cbb_Office.Location = new System.Drawing.Point(206, 213);
+            this.cbb_Office.Name = "cbb_Office";
+            this.cbb_Office.Size = new System.Drawing.Size(204, 21);
+            this.cbb_Office.TabIndex = 13;
+            // 
+            // txb_ComfirmPassword
+            // 
+            this.txb_ComfirmPassword.Location = new System.Drawing.Point(206, 319);
+            this.txb_ComfirmPassword.Name = "txb_ComfirmPassword";
+            this.txb_ComfirmPassword.PasswordChar = '●';
+            this.txb_ComfirmPassword.Size = new System.Drawing.Size(204, 20);
+            this.txb_ComfirmPassword.TabIndex = 14;
+            // 
+            // txb_Password
+            // 
+            this.txb_Password.Location = new System.Drawing.Point(206, 284);
+            this.txb_Password.Name = "txb_Password";
+            this.txb_Password.PasswordChar = '●';
+            this.txb_Password.Size = new System.Drawing.Size(204, 20);
+            this.txb_Password.TabIndex = 15;
+            // 
+            // dtp_DOB
+            // 
+            this.dtp_DOB.Location = new System.Drawing.Point(206, 178);
+            this.dtp_DOB.Name = "dtp_DOB";
+            this.dtp_DOB.Size = new System.Drawing.Size(204, 20);
+            this.dtp_DOB.TabIndex = 16;
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(145, 369);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(75, 23);
+            this.btn_Register.TabIndex = 17;
+            this.btn_Register.Text = "Register";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(293, 369);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 18;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 445);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.dtp_DOB);
+            this.Controls.Add(this.txb_Password);
+            this.Controls.Add(this.txb_ComfirmPassword);
+            this.Controls.Add(this.cbb_Office);
+            this.Controls.Add(this.cbb_Roles);
+            this.Controls.Add(this.txb_Email);
+            this.Controls.Add(this.txb_LastName);
+            this.Controls.Add(this.txb_FirstName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Register";
+            this.Text = "RegisterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txb_FirstName;
+        private System.Windows.Forms.TextBox txb_LastName;
+        private System.Windows.Forms.TextBox txb_Email;
+        private System.Windows.Forms.ComboBox cbb_Roles;
+        private System.Windows.Forms.ComboBox cbb_Office;
+        private System.Windows.Forms.TextBox txb_ComfirmPassword;
+        private System.Windows.Forms.TextBox txb_Password;
+        private System.Windows.Forms.DateTimePicker dtp_DOB;
+        private System.Windows.Forms.Button btn_Register;
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
+
---
+++ Register.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul5A
+{
+    public partial class RegisterForm : Form
+    {
+
+        SqlConnection con;
+        string connection = "Data Source=HAINGUYEN;Initial Catalog=module5a;Integrated Security=True";
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+            loadDataFromDatabase();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Do you want to cancel?", "Alert", MessageBoxButtons.YesNo);
+            if (dr == System.Windows.Forms.DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        public void loadDataFromDatabase()
+        {
+            loadRoles();
+            loadOffice();
+        }
+
+        public void loadRoles()
+        {
+
+            try
+            {
+                con = new SqlConnection(connection);
+                string query = "SELECT Title FROM Roles";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+
+                DataTable dtb = new DataTable();
+                sda.Fill(dtb);
+
+                foreach (DataRow dr in dtb.Rows)
+                {
+                    cbb_Roles.Items.Add(dr["Title"].ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        public void loadOffice()
+        {
+            try
+            {
+                con = new SqlConnection(connection);
+                string query = "SELECT Title from Offices";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+
+                DataTable dtb = new DataTable();
+                sda.Fill(dtb);
+
+                foreach (DataRow dr in dtb.Rows)
+                {
+                    cbb_Office.Items.Add(dr["Title"].ToString());
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            if (validatePass())
+            {
+                try
+                {
+                    con = new SqlConnection(connection);
+                    string query = "INSERT INTO Users VALUES (@roleID, @officeID, @email, @password, @firstName, @lastName, @dateOfBirth, @active)";
+                    SqlCommand scd = new SqlCommand(query, con);
+
+                    scd.Parameters.AddWithValue("@roleID", cbb_Roles.Text);
+                    scd.Parameters.AddWithValue("@officeID", cbb_Office.Text);
+                    scd.Parameters.AddWithValue("@email", txb_Email.Text);
+                    scd.Parameters.AddWithValue("@password", txb_Password);
+                    scd.Parameters.AddWithValue("@password", txb_Password.Text);
+                    scd.Parameters.AddWithValue("@firstName", txb_FirstName.Text);
+                    scd.Parameters.AddWithValue("@lastName", txb_LastName.Text);
+                    scd.Parameters.Add("@dateOfBirth", SqlDbType.Date).Value = dtp_DOB.Text;
+                    scd.Parameters.AddWithValue("@active", 0);
+                    MessageBox.Show("Register successfully");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Password is incorrect. Try again");
+            }
+
+        }
+
+        public bool validatePass()
+        {
+            if (txb_Password.Text == txb_ComfirmPassword.Text)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+}
